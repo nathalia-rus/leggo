ace9cba7e65ba7a435238fbd36dad5b6c685549f
Nathalia Rus <nathaliarus@live.fr>
2018-09-05 16:50:24 +0200

    rotation added

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
 // to start with black screen
 
-TweenMax.to("svg", 2, { opacity: 1 }, 0.5)
+TweenMax.to("svg", 2, { opacity: 1,
+  transformOrigin: '50% 50%' }, 0.5)
+
+
 
 // LINES
 
 // LINES TOP
-var tlinesTop = new TimelineLite();  
+var tlinesTop = new TimelineMax({ repeat: -1 });
 tlinesTop
   .staggerFrom("#FirstLineTop", 25, { 
     fill: "#000000",
@@ -27,9 +30,10 @@ tlinesTop
 
 tlinesTop.timeScale(2);
 
+
 // LINES BOTTOM
 
-var tlinesBottom = new TimelineLite();  
+var tlinesBottom = new TimelineMax({ repeat: -1 });
 
 tlinesBottom
   .staggerFrom("#BottomLineBottom", 25, {
@@ -52,36 +56,44 @@ tlinesBottom
 tlinesBottom.timeScale(2);
 
 
-var tl = new TimelineLite(); 
+var tl = new TimelineMax();
 
 tl
 .staggerFrom(".FirstLeft", 1, {
   drawSVG: 0,
+  rotation: 360,
   delay: 1
   }, 0.02, "-10")
 .staggerFrom(".SecondLeft", 1, {
-  drawSVG: 0
+  drawSVG: 0,
+  rotation: 360,
   }, 0.01, "-=18")
 .staggerFrom(".ThirdLeft", 1, {
-  drawSVG: 0
+  drawSVG: 0,
+  rotation: 360
   }, 0.01, "-=16")
   .staggerFrom(".FourthLeft", 1, {
   drawSVG: 0
   }, 0.01, "-=17")
 .staggerFrom(".FifthLeft", 1, {
-  drawSVG: 0
+  drawSVG: 0,
+  rotation: 360
   }, 0.01, "-=15")
   .staggerFrom(".FirstRight", 1, {
-  drawSVG: 0
+  drawSVG: 0,
+    rotation: 360
   }, 0.01, "-=14")
   .staggerFrom(".SecondRight", 1, {
-  drawSVG: 0
+  drawSVG: 0,
+    rotation: 360
   }, 0.01, "-=13" )
   .staggerFrom(".ThirdRight", 1, {
-  drawSVG: 0
+  drawSVG: 0,
+    rotation: 360
   }, 0.01, "-=12")
   .staggerFrom(".FourthRight", 1, {
   drawSVG: 0,
+    rotation: 360
   }, 0.01, "-=20").tl.reverse();
 
 tl.timeScale(2);
